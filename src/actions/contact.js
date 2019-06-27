eb243344089dcfcd5b53add344a3ce2abf6f493a
Kiran Adhikari <kiruu1238@gmail.com>
2019-06-27 08:49:30 +0545

    Actions updated

--- src/actions/contact.js
+++ src/actions/contact.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
 
-
-
 export const contactMessage = (contactData) => async dispatch => {
     const res = await axios.post('http://localhost:5000/contact_us', contactData);
     console.log(res)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,11 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
-<<<<<<< HEAD
 import contactReducer from './contactReducer';
-=======
->>>>>>> 3fceaf55aa0d13a822c447b73d674b603758dd31
 import errorReducer from './errorReducer';
 
 
 export default combineReducers({
   auth: authReducer,
   errors: errorReducer,
-<<<<<<< HEAD
   contact: contactReducer
-=======
->>>>>>> 3fceaf55aa0d13a822c447b73d674b603758dd31
 });
